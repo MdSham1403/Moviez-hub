4a060ec8c9298168eff9125d235c8ffa8d1e9a15
MdSham1403 <mdsham1403@gmail.com>
2026-02-28 04:58:47 +0530

    Final Fix: Integrated Moviezhub folder

--- Moviezhub/Moviezhub/settings.py
+++ Moviezhub/Moviezhub/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY", default="unsafe-dev-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1"]
 
